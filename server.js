843bbc2561bddc4b435bd182a0c8aac4559e5578
Aravind <aravind16101800@gmail.com>
2025-01-27 19:52:00 +0530

    Fixed port issue and updated revenue calculations

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { google } = require("googleapis");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
